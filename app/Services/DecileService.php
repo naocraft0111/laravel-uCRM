e82e3c7f1116ac5d9e01172a656c0407099a5d9a
naocraft0111 <naoki1306@gmail.com>
2022-10-14 21:46:41 +0900

    decile

--- app/Services/DecileService.php
+++ app/Services/DecileService.php
@@ -1,53 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use Inertia\Inertia;
-use App\Models\Order;
+namespace App\Services;
 use Illuminate\Support\Facades\DB;
 
-class AnalysisController extends Controller
+class DecileService
 {
-    public function index()
+    public static function decile($subQuery)
     {
-        $startDate = '2022-08-01';
-        $endDate = '2022-08-31';
-
-        // $period = Order::betweenDate($startDate, $endDate)
-        // ->groupBy('id')
-        // ->selectRaw('id, sum(subtotal) as total,
-        // customer_name, status, created_at')
-        // ->orderBy('created_at')
-        // ->paginate(50);
-
-        // dd($period);
-
-        // $subQuery = Order::betweenDate($startDate, $endDate)
-        // ->where('status', true)
-        // ->groupBy('id')
-        // ->selectRaw('id, SUM(subtotal) as totalPerPurchase,
-        // DATE_FORMAT(created_at, "%Y%m%d") as date');
-
-        // $data = DB::table($subQuery)
-        // ->groupBy('date')
-        // ->selectRaw('date, sum(totalPerPurchase) as total' )
-        // ->get();
-
-        // dd($data);
-
-        return Inertia::render('Analysis');
-    }
-
-    // 記録用
-    public function decile()
-    {
-        $startDate = '2022-08-01';
-        $endDate = '2022-08-31';
-
         // 1. 購買ID毎にまとめる
-        $subQuery = Order::betweenDate($startDate, $endDate)
-        ->groupBy('id')
+        $subQuery = $subQuery->groupBy('id')
         ->selectRaw('id, customer_id, customer_name,
         SUM(subtotal) as totalPerPurchase');
 
@@ -59,13 +20,13 @@ class AnalysisController extends Controller
 
         // 3. 購入順に連番を振る
         DB::statement('set @row_num = 0;');
-            $subQuery = DB::table($subQuery)
-            // 変数に+1したものを変数に代入し、row_numと言う列で表示する
-            ->selectRaw('
-            @row_num:= @row_num+1 as row_num,
-            customer_id,
-            customer_name,
-            total');
+        $subQuery = DB::table($subQuery)
+        // 変数に+1したものを変数に代入し、row_numと言う列で表示する
+        ->selectRaw('
+        @row_num:= @row_num+1 as row_num,
+        customer_id,
+        customer_name,
+        total');
 
 
         // dd($subQuery);
@@ -133,6 +94,10 @@ class AnalysisController extends Controller
         ')
         ->get();
 
-        // dd($data);
+        // グラフの縦横
+        $labels = $data->pluck('decile');
+        $totals = $data->pluck('totalPerGroup');
+
+        return [$data, $labels, $totals];
     }
 }
